1ef9586c2e1550cbb8dcaa5efab196c44225593b
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-03-06 14:51:20 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const porta = 3000;
-
 const app = express();
 
-app.get('/listaAluno', (req, res) => {
-    let arrAluno = ["Giovanna", "Raphael", "Amanda"];
-    let texto = "";
+const routes = require('./src/routes/routes');
 
-    for (i = 0; i < arrAluno.length; i++) {
-        texto += i + ":" + arrAluno[i] + "<br>";
-    }
+app.use('/', routes);
 
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8' 
-    });
-    res.end(texto);
-});
 app.listen(porta, () => {
-    console.log("Servidor ok...")
+    console.log("Servidor executado na porta 3000");
 });
---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+//define o processo de rota no framework
+const router = express.Router();
+
+router.get('/listaAluno', (req, res) =>{
+    let arrAluno = ["Giovanna", "Raphael", "Amanda"];
+    let texto = "";
+
+    for (i = 0; i < arrAluno.length; i++) {
+        texto += i + ":" + arrAluno[i] + "<br>";
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8' 
+    });
+    res.end(texto);
+});
+//exporta o arquivo para o modulo de exportação, que executa os arquivos externos em que conjunto com o framework
+module.exports = router;
