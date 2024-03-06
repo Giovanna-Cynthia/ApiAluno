04ab84dd168fe419e97d0ed6c49467d7bdff796e
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-03-06 15:33:22 -0300

    atualiza

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const porta = 3000;
 const app = express();
+
+const indexView = require("./src/view/indexView")
 
 const routes = require('./src/routes/routes');
 
@@ -9,3 +11,8 @@ app.use('/', routes);
 app.listen(porta, () => {
     console.log("Servidor executado na porta 3000");
 });
+
+app.use(express.static("Public"))
+app.get('/formulario', (req, res, next) => {
+    res.end(indexView())
+})
