90c50b84b0f1810a0e607e2337c39ae1382cb9de
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-03-12 16:38:17 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -33,11 +33,26 @@ router.get('/AlunoNome/:name', (req, res) => {
     if (index > -1) {
         texto = "<h1>Aluno localizado no sistema.</h1> <h3>O id do Aluno no Array é" + index + "</h3>";
     } else{
-        texto = "<h1>Aluno não localizaod no sistema </h1>";
+        texto = "<h1>Aluno não localizado no sistema </h1>";
     }
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.end(texto);
 })
+
+router.get('/cadastrarAluno/:name', (req, res) => {
+    const { name } = req.params;
+
+    const index = arrAluno.map(aluno => aluno.name).indexOf(name);
+
+    if(index === -1) {
+        let novoNome = {name: name};
+        arrAluno.push(novoNome);
+        res.send(arrAluno);
+    }else{
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.end("<h1>Usuário ja cadastrado</h1>");
+    }
+});
 
 
 //exporta o arquivo para o modulo de exportação, que executa os arquivos externos em conjunto com o framework
