4c6648bdd8d4b31cac57b939bc943e2d9f3f0317
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-03-12 14:36:36 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,11 +12,11 @@ router.get('/listaAluno', (req, res) =>{
     let texto = "";
 
     for (i = 0; i < arrAluno.length; i++) {
-        texto += i + ":" + arrAluno[i] + "<br>";
+        //texto += i + ":" + arrAluno[i] + "<br>";
+        texto += i + ":" + Object.values(arrAluno[i]) + "<br>"
     }
 
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8' 
-    });
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8' });
     res.end(texto);
 });
 
@@ -25,6 +25,19 @@ const indexView = require("../view/indexView")
 router.get('/formulario', (req, res, next) => {
     res.send(indexView())
 });
+
+router.get('/AlunoNome/:name', (req, res) => {
+    const {name} = req.params;
+    const index = arrAluno.map(aluno => aluno.name).indexOf(name);
+    let texto;
+    if (index > -1) {
+        texto = "<h1>Aluno localizado no sistema.</h1> <h3>O id do Aluno no Array é" + index + "</h3>";
+    } else{
+        texto = "<h1>Aluno não localizaod no sistema </h1>";
+    }
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(texto);
+})
 
 
 //exporta o arquivo para o modulo de exportação, que executa os arquivos externos em conjunto com o framework
