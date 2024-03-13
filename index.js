a36ca2ad72cc1a224277e4b2e3708995039b6b52
Giovanna-Cynthia <154370247+Giovanna-Cynthia@users.noreply.github.com>
2024-03-13 14:47:24 -0300

    instalacao do Body-Parser

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
+const bodyParser = require('express');
+
 const porta = 3000;
 const app = express();
 app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const routes = require('./src/routes/routes');
 
